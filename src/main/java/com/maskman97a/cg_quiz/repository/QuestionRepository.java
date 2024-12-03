9a98b2124be315b0f94893ec51b2bf35c25384cd
tungpt46 <tungpt46@viettel.com.vn>
2024-12-03 20:36:55 +0700

    Update search question

--- src/main/java/com/maskman97a/cg_quiz/repository/QuestionRepository.java
+++ src/main/java/com/maskman97a/cg_quiz/repository/QuestionRepository.java
@@ -1,11 +1,13 @@
 package com.maskman97a.cg_quiz.repository;
 
+import com.maskman97a.cg_quiz.dto.enums.QuestionDifficultEnum;
 import com.maskman97a.cg_quiz.entity.QuestionEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
 import java.util.List;
@@ -34,6 +36,17 @@ public interface QuestionRepository extends JpaRepository<QuestionEntity, Long> 
             WHERE e.id = :examId
             """)
     List<QuestionEntity> findByExamId(Long examId);
+
     Page<QuestionEntity> findAll(Pageable pageable);
-    List<QuestionEntity> findByTitleContaining(String title);
+
+
+    List<QuestionEntity> findByTitleContainingIgnoreCase(String title);
+
+    @Query("""
+            SELECT q
+            FROM QuestionEntity q
+            WHERE q.title LIKE %:keyword%
+            AND (:difficult is null or q.difficult = :difficult)
+            """)
+    List<QuestionEntity> findByTitleContaining(String keyword, QuestionDifficultEnum difficult);
 }
